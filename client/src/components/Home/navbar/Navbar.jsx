29aa6cc39c14bf3fe808fb2861c5bc68ce297efd
Armando Martinez <alemar.martinez16@gmail.com>
2023-06-06 11:40:38 -0300

    Estilos Navbar.jsx

--- client/src/components/Home/navbar/Navbar.jsx
+++ client/src/components/Home/navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Search from "./Search";
-import logonav from "./Logo/tasty_cooking.png";
+import logonav from "./Logo/tasty_cooking_SF.png";
 import { Link } from "react-router-dom";
 import github from "../../../assets/img/github.png";
 import linkedith from "../../../assets/img/linkedin.png";
@@ -17,17 +17,12 @@ const Navbar = () => {
   return (
     <nav>
       {/* ------------LOGO------------------------------------------------------ */}
-      <div className="logo">
-        <div className="imgen__logo">
-          {/*  <img
-            src={require("./logo/tasty_cooking.png")}
-            alt="Logo Not Found"
-            width="50px"
-            height="50px"
-          /> */}
-          <img src={logonav} alt="Logo Not Fount" />
+
+        <div className="imagen__logo">
+        <img src={logonav} alt="Logo Not Fount"
+          width="200px"
+          height="50px"/>
         </div>
-      </div>
 
       {/* ----------Input buscador de Recetas------------------------------------- */}
       <div className="nav__search">
